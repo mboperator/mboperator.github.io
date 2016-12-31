d2bf90fdc44c872cb08eae7c095e742bc33790d4
Marcus Bernales <mboperator@gmail.com>
2016-12-31 00:55:49 -0800

    Center justified name and subtitle

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,13 +7,28 @@ import SiteSection from '../components/SiteSection';
 import scrollDecorator from '../decorators/pageScroll';
 
 const TrapTears = () => (
-  <div style={{ height: '100%', width: '100%', position: 'relative' }}>
+  <div style={{
+    height: '100%',
+    width: '100%',
+    position: 'relative',
+  }}>
     <video autoPlay loop>
       <source src="/musings.mp4" type="video/mp4" />
     </video>
-    <div className="App-heading">
-      <h1>Marcus Bernales</h1>
-      <h3>reflections and thoughts</h3>
+    <div style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <div>
+        <h1>M. Bernales</h1>
+        <h3>reflections and thoughts</h3>
+      </div>
     </div>
   </div>
 );
@@ -25,11 +40,17 @@ const Enlighten = () => (
       width: '100%',
       backgroundImage: 'url(/opensource.jpg)',
       backgroundSize: 'cover',
-      position: 'relative',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
     }}
   >
-    <div className="App-heading">
-      <h1>Marcus Bernales</h1>
+    <div style={{
+      backgroundColor: 'black',
+      color: 'rgba(255, 255, 255, 0.66)',
+      mixBlendMode: 'exclusion',
+    }}>
+      <h1>M. Bernales</h1>
       <h3>technology for humans</h3>
     </div>
   </div>
